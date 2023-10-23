8a825b03ac1a0fbaff71e9f573ca12b8a6cc5138
GOOHAESEUNG <amu6675@naver.com>
2023-10-24 00:58:46 +0900

    develop/join

---
+++ src/main/java/com/swuProject/secound/SecoundApplication.java
@@ -0,0 +1,13 @@
+package com.swuProject.secound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecoundApplication.class, args);
+	}
+
+}
