35cb4af0120e719bbae94fea6f1342e9485cbbf8
yushinc <cys74351@gmail.com>
2023-11-26 02:15:02 +0900

    fix: cors 시큐리티 수정

--- src/main/java/com/swuProject/secound/config/WebMvcConfig.java
+++ src/main/java/com/swuProject/secound/config/WebMvcConfig.java
@@ -28,7 +28,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
                         .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
                         .exposedHeaders("Authorization", "RefreshToken")
-                        .allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
+                        .allowCredentials(true);
+                        // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
+                        // 이 메소드는 와일드 카드 설정 시 사용 불가능, 특정 도메인 지정 시 사용 가능 이므로 에러나는거
             }
         };
 
