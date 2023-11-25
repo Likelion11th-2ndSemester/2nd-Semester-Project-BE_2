7fbd05de9cd2efbc2f8372cb1c526135cda70fbf
yushinc <cys74351@gmail.com>
2023-11-26 01:16:27 +0900

    fix: cors 설정 수정

--- src/main/java/com/swuProject/secound/repository/HashtagRepositoryCustom.java
+++ src/main/java/com/swuProject/secound/repository/HashtagRepositoryCustom.java
@@ -9,5 +9,5 @@ import java.util.List;
 
 @Repository
 public interface HashtagRepositoryCustom {
-    List<Photo> findPhotosByMemberUsername(String username);
+    List<Photo> findPhotosByMemberNickname(String nickname);
 }
