454997b234e3baf439af8a5a22cc345ed49956f8
yushinc <cys74351@gmail.com>
2023-11-26 02:55:04 +0900

    fix: cors 설정 변경

--- src/main/java/com/swuProject/secound/config/WebConfig.java
+++ src/main/java/com/swuProject/secound/config/WebConfig.java
@@ -3,12 +3,13 @@ package com.swuProject.secound.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     @Value("${uploadPath}")
     String uploadPath;
 
@@ -18,13 +19,23 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations(uploadPath);
     }
 
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .exposedHeaders("X-AUTH-TOKEN")
-                .allowCredentials(true)
-                .allowedOrigins("http:localhost:3000");
-    }
+                .allowedOrigins("http://localhost:3000") // React 애플리케이션의 도메인
+                .allowedHeaders("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name()
+                )
+                .exposedHeaders("Authorization", "RefreshToken", "X-AUTH-TOKEN")
+                .allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
+    };
+
 
 
 }
