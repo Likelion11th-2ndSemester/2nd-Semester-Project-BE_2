b64533dda2aa07160dfc217ad88a57e2ca5a7b14
yushinc <cys74351@gmail.com>
2023-11-26 21:54:10 +0900

    chore: 태그된 친구 검색 시 이름이 아니라 닉네임으로 검색하도록 로직 수정

--- src/main/java/com/swuProject/secound/repository/MemberRepository.java
+++ src/main/java/com/swuProject/secound/repository/MemberRepository.java
@@ -10,8 +10,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
-    Member findByName(String username);
-
     Member findByNickname(String nickname);
 
 }
